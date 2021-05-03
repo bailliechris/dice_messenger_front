88bfe7530abd1058745812493c52ff6990442729
bailliechris <bailliechristopher@gmail.com>
2021-05-03 21:17:15 +0100

    Added userlist

--- src/components/MessageArea.vue
+++ src/components/MessageArea.vue
@@ -11,7 +11,13 @@
           <b-input v-model="msg_send"></b-input>
       </b-field>
       <section>
-          <b-button @click="ws_send">Send</b-button>
+        <b-button @click="ws_send">Send</b-button>
+      </section>
+      <section class="box">
+        <p>Users Connected:</p>
+        <p v-for="(name, index) in all_users"
+        v-bind:key="index"
+        >{{ name }}</p> 
       </section>
     </div>
     <div v-else>
@@ -40,24 +46,46 @@ export default {
       temp_name: "",
       msg_send: "",
       all_posts: [],
+      all_users: [],
       ws: null
     }
   },
   created() {
     // Initialise the ws connection
     console.log("Starting connection to WebSocket Server");
-    this.ws = new WebSocket('wss://dice-messenger.herokuapp.com/');
+    //this.ws = new WebSocket('wss://dice-messenger.herokuapp.com/');
+    //this.ws = new WebSocket('ws://localhost:3000');
   },
   watch: {
     ws: function() {
-      this.ws.onmessage = (event) =>  {
-          console.log("Received a message");
-          let message = JSON.parse(event.data);
-//          console.log(message);
-          this.all_posts.push(message);
+      this.ws.onclose = () => {
+        this.ws.close(1000, this.name);
       }
 
-      this.ws.onopen = (event) => {
+      this.ws.onmessage = (event) =>  {
+        let message = JSON.parse(event.data);
+        console.log(message);
+        switch(message.type) {
+          case "open":
+            console.log("Updating Users");
+            this.all_users = message.msg;
+            break;
+          
+          case "message":
+            console.log("Receiving a message");
+            this.all_posts.push(message);
+            break;
+
+          case "close":
+
+            break;
+
+          default:
+            console.log("No type found, ignoring.")            
+        }
+      }
+
+      this.ws.onopen = () => {
         let data = {
           name: this.name,
           type: "open",
@@ -65,7 +93,8 @@ export default {
           time: Date.now()
         }
 
-        let sent = JSON.stringify(data)
+        let sent = JSON.stringify(data);
+        console.log(sent);
         this.ws.send(sent);
       }
     }
@@ -73,10 +102,12 @@ export default {
   methods: {
       confirm_name: function() {
         this.name = this.temp_name;
+        this.ws = new WebSocket('ws://localhost:3000');
       },
       ws_send: function () {
         let data = {
           name: this.name,
+          type: "message",
           msg: this.msg_send,
           time: Date.now()
         }
